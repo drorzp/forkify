ba1f0b690bfaa6171bcb0345166750ce0845ca31
Dror Zeplovitch <drorzp@hotmail.com>
2020-12-16 19:45:39 -0500

    adding

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,17 +1,17 @@
 class SearchView {
-  #parentEl = document.querySelector('.search');
+  _parentEl = document.querySelector('.search');
   getQuery() {
-    const search = this.#parentEl.querySelector('.search__field').value;
+    const search = this._parentEl.querySelector('.search__field').value;
     this._clearInput();
     return search;
   }
 
   _clearInput() {
-    this.#parentEl.querySelector('.search__field').value = '';
+    this._parentEl.querySelector('.search__field').value = '';
   }
 
   addHandlerSearch(cb) {
-    this.#parentEl.addEventListener('submit', function (e) {
+    this._parentEl.addEventListener('submit', function (e) {
       e.preventDefault();
       cb();
     });
